7e92e9a075e72b7154e4b8eb39ed44f1487eee1f
Eddie Hsu <aoe102198@gmail.com>
2022-04-18 21:45:47 +0800

    Import 'styled-components' to style the Header

--- Sophie-Shopping/frontend/components/Header.js
+++ Sophie-Shopping/frontend/components/Header.js
@@ -1,23 +1,51 @@
 import Link from 'next/link';
-import PropTypes from 'prop-types';
+
+import styled from 'styled-components';
 
 import Nav from './Nav';
 
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+const HeaderStyle = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
+
 export default function Header() {
   return (
-    <header>
+    <HeaderStyle>
       <div className="bar">
-        <Link href="/">Sophie Shopping</Link>
+        <Logo>
+          <Link href="/">Sophie Shopping</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyle>
   );
 }
-
-// Header.propTypes = {
-//   children: PropTypes.any,
-//   cool: PropTypes.string,
-// };
