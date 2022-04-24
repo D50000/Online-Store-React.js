512eab42b28795ce2464c7c1c083a43fd9a5adc3
Eddie Hsu <aoe102198@gmail.com>
2022-04-24 14:26:26 +0800

    Integrate with ApolloProvider in MyApp and config the 'getInitialProps' with next.js

--- Sophie-Shopping/frontend/pages/_app.js
+++ Sophie-Shopping/frontend/pages/_app.js
@@ -1,25 +1,42 @@
 import { Router } from 'next/router';
 import PropTypes from 'prop-types';
 import NProgress from 'nprogress';
+import { ApolloProvider } from '@apollo/client';
 import 'nprogress/nprogress.css'; // custom style instead
 
 import '../components/styles/nprogress.css';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 // Higher priority order _document > _app.js > index.js
-export default function MyApp({ Component, props }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component />
+      </Page>
+    </ApolloProvider>
   );
 }
 
+// Next.js initial feature. Setup the initial App state.
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
+
 MyApp.propTypes = {
   Component: PropTypes.any,
-  props: PropTypes.any,
+  pageProps: PropTypes.any,
+  apollo: PropTypes.any,
 };
