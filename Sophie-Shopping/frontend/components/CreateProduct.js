01dd787be9048a10a4db44bd49d92ff70a17bf23
Eddie Hsu <aoe102198@gmail.com>
2022-04-30 18:03:15 +0800

    Import the 'Router' and redirect to the specific product that just created.

--- Sophie-Shopping/frontend/components/CreateProduct.js
+++ Sophie-Shopping/frontend/components/CreateProduct.js
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
+import Router from 'next/router';
 
 import Form from './styles/Form';
 import DisplayError from './ErrorMessage';
@@ -53,8 +54,11 @@ export default function CreateProduct() {
         console.log(inputs);
         e.preventDefault();
         // Destructuring into [createProduct, { loading, error, data }] with the latest state.
-        await createProduct();
+        const res = await createProduct();
         clearForm();
+        Router.push({
+          pathname: `/product/${res.data.createProduct.id}`,
+        });
       }}
     >
       <DisplayError error={error} />
