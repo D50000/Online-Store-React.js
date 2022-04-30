441f85017f94999b3c94e8af47a3f136e972db68
Eddie Hsu <aoe102198@gmail.com>
2022-04-30 17:28:01 +0800

    Create the 'mutation' to add the product

--- Sophie-Shopping/frontend/components/CreateProduct.js
+++ Sophie-Shopping/frontend/components/CreateProduct.js
@@ -1,23 +1,62 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
 import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
 
 import useForm from '../lib/useForm';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   // Construct then hook the response to the function.
   const { inputs, handleChange, resetForm, clearForm } = useForm({
-    name: 'test name',
-    price: '1234',
-    description: 'test test ',
+    name: '',
+    price: 0,
+    description: '',
   });
+
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
 
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         console.log(inputs);
         e.preventDefault();
+        // Destructuring into [createProduct, { loading, error, data }] with the latest state.
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input type="file" id="image" name="image" onChange={handleChange} />
