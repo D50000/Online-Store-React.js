dc27b7ed9af35d14c9e9d25f1e651a1b89a49929
Eddie Hsu <aoe102198@gmail.com>
2022-05-06 00:27:22 +0800

    Add the Logout feature
    - Add the SignOut.js component add insert in the navigation bar.
    - Use 'mutation {endSession}' to delete the session and use 'refetchQueries' to re-fetch the data.

--- Sophie-Shopping/frontend/components/Nav.js
+++ Sophie-Shopping/frontend/components/Nav.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 
 import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
+import SignOut from './SignOut';
 
 export default function Nav() {
   const user = useUser();
@@ -15,6 +16,7 @@ export default function Nav() {
           <Link href="/sell">Sell</Link>
           <Link href="/orders">Orders</Link>
           <Link href="/account">Account</Link>
+          <SignOut />
         </>
       )}
       {!user && <Link href="/signin">Sign In</Link>}
---
+++ Sophie-Shopping/frontend/components/SignOut.js
@@ -0,0 +1,22 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
+import { CURRENT_USER_QUERY } from './User';
+
+const SIGNIN_OUT_MUTATION = gql`
+  mutation {
+    endSession
+  }
+`;
+
+export default function SignOut() {
+  const [signout] = useMutation(SIGNIN_OUT_MUTATION, {
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+
+  return (
+    <button type="button" onClick={signout}>
+      Sign Out ~
+    </button>
+  );
+}
