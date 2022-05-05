ba256ba2dd8a39c692df1021038ae763debec1d1
Eddie Hsu <aoe102198@gmail.com>
2022-05-05 23:59:46 +0800

    Create login feature
    - Add SignInPage for the router
    - Add the SignIn.js component.

--- Sophie-Shopping/frontend/pages/signin.js
+++ Sophie-Shopping/frontend/pages/signin.js
@@ -1,9 +1,5 @@
-
+import SignIn from '../components/SignIn';
 
 export default function SignInPage() {
-  return (
-    <di>
-      <p>Sign on In ~~~~~~~</p>
-    </di>
-  )
+  return <SignIn />;
 }
