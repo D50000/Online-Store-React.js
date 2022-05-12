7abefdfc32320fcb8a34d885a54219d2227f1cb2
Eddie Hsu <aoe102198@gmail.com>
2022-05-12 22:07:29 +0800

    Add remove button in the cart and use "mutation deleteCartItem" for the implement.

--- Sophie-Shopping/frontend/components/Cart.js
+++ Sophie-Shopping/frontend/components/Cart.js
@@ -9,6 +9,7 @@ import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
 import { useUser } from './User';
+import RemoveFromCart from './RemoveFromCart';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -43,6 +44,7 @@ function CartItem({ cartItem }) {
           </em>
         </p>
       </div>
+      <RemoveFromCart id={cartItem.id} />
     </CartItemStyles>
   );
 }
